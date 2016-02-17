94e83b0af0ee7d7b9e27b285b0ca27528da19051
John <john@real-currents.com>
2016-02-17 13:35:27 -0800

    update backup cronjob

--- opsworks_nodejs/recipes/backup.rb
+++ opsworks_nodejs/recipes/backup.rb
@@ -1,6 +1,6 @@
 cron "feedback_data_backup" do
     day "*"
-    hour "21"
+    hour "23"
     minute "30"
-    command "pg_dump --host=localhost --port=5432 --username=postgres --no-password --format=c --file=/home/data/${maponics_feedback}-$(date '+%a-%b%d-%Y-%H.%M.%S.%Z').backup maponics_feedback"
+    command "pg_dump --host=localhost --port=5432 --username=postgres --no-password --format=c --file=/home/data/maponics_feedback-$(date '+%a-%b%d-%Y-%H.%M.%S.%Z').backup maponics_feedback"
 end
